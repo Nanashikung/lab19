225b10d3e1410809a1e0bd17372ce930b8517aae
Nanashikung <nata.station@hotmail.com>
2022-01-25 22:51:13 +0700

    lab19_1.cpp complete

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 #include<string>
 #include<cstdlib>
+#include<cstring>
 
 using namespace std;
 
@@ -20,20 +21,66 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string f, vector<string> &n, vector<int> &s, vector<char> &g){
+    ifstream x(f);
+    string l;
+    do {
+        getline(x, l);
+        if (!l.length()) break;
+        char name[30] = {};
+        int scores[3] = {}, sum = 0;
+        sscanf(l.c_str(), "%[^:]", name);
+        sscanf(l.c_str()+strlen(name)+2, "%d %d %d", scores, scores+1, scores+2);
+        for (int i = 0;i < 3; ++i) sum += scores[i];
+        n.push_back(string(name));
+        s.push_back(sum);
+        g.push_back(score2grade(sum));
+    } while(l.length());
+    x.close();
 }
 
-void getCommand(){
-
+void getCommand(string &c, string &k){
+    string cmd;
+    cout << "Please input your command: ";
+    getline(cin, cmd);
+    unsigned int j = 0;
+    for (unsigned int i = 0; i < cmd.length(); ++i) {
+        if (cmd[i] == ' ') {
+            cmd[i] = '\0';
+            j = i + 1;
+            break;
+        }
+    }
+    c = string(&cmd[0]);
+    if (j) k = string(&cmd[j]);
 }
 
-void searchName(){
+void searchName(const vector<string> &n, const vector<int> &s, const vector<char> &g, const string k){
+    cout << "---------------------------------\n";
+    int size=n.size();
+    bool checkname=false;
+    for (int i = 0; i < size; ++i) {
+        if (toUpperStr(n[i]) == k) {
+            cout << n[i] << "'s score = " << s[i] << endl;
+            cout << n[i] << "'s grade = " << g[i] << endl;
+            checkname=true;
+        }
+        
+        }
 
+    if (!checkname) cout << "Cannot found." << endl;
+    cout << "---------------------------------\n";
 }
 
-void searchGrade(){
-
+void searchGrade(const vector<string> &n, const vector<int> &s, const vector<char> &g, const string k){
+    cout << "---------------------------------\n";
+    int size=g.size();
+    for (int i = 0; i < size; ++i) {
+        if (g[i] == k[0]) {
+            cout << n[i] << " (" << s[i] << ")\n";
+        }
+    }
+    cout << "---------------------------------\n";
 }
 
 
